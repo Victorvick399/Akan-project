026bbc247d7f580b95c0772b799f15306ab04e00
Victorvick399 <victorvick399@gmail.com>
2019-09-23 08:15:10 +0300

    Add function

--- js/main.js
+++ js/main.js
@@ -12,4 +12,9 @@ var dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday",
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
+ dayOfTheWeek = (((century/4)-2*century-1)+((5*year/4))+((25*(month+1)/10))+day)%7
+
+
+
+
  
