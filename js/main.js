af2ecc43afa380bc59e30c1a33f1594c2138c6dc
Victorvick399 <victorvick399@gmail.com>
2019-09-23 13:32:29 +0300

    Add switch and case

--- js/main.js
+++ js/main.js
@@ -1,20 +1,66 @@
-var century,year,month,day
+
 
 function receiveInput(){
-    var day =  parseInt(document.getElementById("day").Value );
-    var month = parseInt(document.getElementById("month").Value);
-    var year = parseInt(document.getElementById("year").Value);
-    var century = parseInt(document.getElementById("century").Value);
-    var gender = parseInt(document.getElementById("gender").Value);
+    var day =  parseInt(document.getElementById("day").value) ;
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+    var century = parseInt(document.getElementById("century").value);
+    var gender = parseInt(document.getElementById("gender").value);
+    alert(day);
+    
+    var dayOfTheWeek = Math.floor((((century/4)-2*century-1)+((5*year/4))+((25*(month+1)/10))+day) % 7 );
+   
 }
 
-var dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+var nameOfTheDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+ 
+switch(dayOfTheWeek){
+    case(0):
+    if(gender="male"){
+        alert(maleNames[0])
+    }else{
+        alert(femaleNames[0])
+    }
+    case(1):
+    if(gender="male"){
+        alert(maleNames[1])
+    }else{
+        alert(femaleNames[1])
+    }
+    case(2):
+    if(gender="male"){
+        alert(maleNames[2])
+    }else{
+        alert(femaleNames[2])
+    }
+    case(3):
+    if(gender="male"){
+        alert(maleNames[3])
+    }else{
+        alert(femaleNames[3])
+    }
+    case(4):
+    if(gender="male"){
+        alert(maleNames[4])
+    }else{
+        alert(femaleNames[4])
+    }
+    case(5):
+    if(gender="male"){
+        alert(maleNames[5])
+    }else{
+        alert(femaleNames[5])
+    }
+    case(6):
+    if(gender="male"){
+        alert(maleNames[6])
+    }else{
+        alert(femaleNames[6])
+    }
 
- dayOfTheWeek = (((century/4)-2*century-1)+((5*year/4))+((25*(month+1)/10))+day)%7
 
-
-
-
+}
  
