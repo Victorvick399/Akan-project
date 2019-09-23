162fd72311fd6a880dd1fca7b584690d07aea043
Victorvick399 <victorvick399@gmail.com>
2019-09-23 07:18:16 +0300

    Add function to main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var century,year,month,day
+
+function receiveInput(){
+    var day =  parseInt(document.getElementById("day").Value );
+    var month = parseInt(document.getElementById("month").Value);
+    var year = parseInt(document.getElementById("year").Value);
+    var century = parseInt(document.getElementById("century").Value);
+    var gender = parseInt(document.getElementById("gender").Value);
+}
